1902ff5f65f8e86beb4bc4743ccb087242b7de20
Jens Sauer <jens@Jenss-MacBook-Pro.local>
2018-01-19 20:53:52 +0100

    initial commit

---
+++ RangeSliderTest/RangeSliderTest.fs
@@ -0,0 +1,15 @@
+﻿namespace RangeSliderTest
+
+open Xamarin.Forms
+open Xamarin.RangeSlider.Forms
+
+type App() =
+    inherit Application()
+
+    let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
+    let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
+    let rangeSlider = RangeSlider(MinimumValue = 0.f, MaximumValue = 100.f, LowerValue = 10.f, UpperValue = 90.f, HeightRequest = 10.0, WidthRequest = 100.0)
+    do
+        stack.Children.Add(label)
+        stack.Children.Add(rangeSlider)
+        base.MainPage <- ContentPage(Content = stack)
---
+++ iOS/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿namespace RangeSliderTest.iOS
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("RangeSliderTest.iOS")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+[<assembly: Xamarin.Forms.ExportRenderer(typedefof<Xamarin.RangeSlider.Forms.RangeSlider>, typedefof<Xamarin.RangeSlider.Forms.RangeSliderRenderer>)>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
